aca7dec209af78f7b97322f6f3ce928eb49a46d0
jl237561 <mr.li.jinpeng@gmail.com>
2018-03-20 21:06:26 +0100

    beta version which works

--- cached_sklearn/model.py
+++ cached_sklearn/model.py
@@ -8,9 +8,13 @@ Created on Mon Mar 19 23:46:18 2018
 import os
 import numpy as np
 import hashlib
+import joblib
+import copy
 
 def get_cache_dir():
-    cache_dir = os.path.expanduser("~/tmp/talkdata")
+    cache_dir = os.path.expanduser("~/.cached_sklearn/cached")
+    if not os.path.isdir(cache_dir):
+        os.makedirs(cache_dir)
     return cache_dir
 
 def get_np_arrays_hash(arrays):
@@ -31,21 +35,38 @@ def get_np_arrays_hash(arrays):
 def create_cached_model(Model, **kwargs):
     class CachedModel(Model):
         def __init__(self, **kwargs):
-            self.cm_hash_key = hash(str(Model) + repr(kwargs))
-            self.cm_cache_dir = get_cache_dir()
+            self._cm_hash_key = hash(str(Model) + repr(kwargs))
+            self._cm_cache_dir = get_cache_dir()
+            self._cm_Model = Model
+            super(CachedModel, self).__init__(**kwargs)
 
-        def fit(self, X, y):
-            Xy_hash = get_np_arrays_hash([X, y])
-            fitted_model_hash = str(self.model_hash_key) + str(Xy_hash)
+        def fit(self, X, y, **kwargs):
+            array_hash = get_np_arrays_hash([X, y])
+            fitted_model_hash = str(self._cm_hash_key) + str(array_hash) + str(kwargs)
             m = hashlib.md5()
             m.update(fitted_model_hash.encode("utf-8"))
             fitted_model_hash = str(m.hexdigest())
+            path_trained_model_dir = os.path.join(self._cm_cache_dir, fitted_model_hash)
+            path_trained_model = os.path.join(path_trained_model_dir, "model.joblib")
+            if os.path.isfile(path_trained_model):
+                cached_model = joblib.load(path_trained_model)
+                for attr_name in cached_model.__dict__:
+                    try:
+                        setattr(self, attr_name, getattr(cached_model, attr_name))
+                    except:
+                        pass
+            else:
+                super(CachedModel, self).fit(X, y, **kwargs)
+                if not os.path.isdir(path_trained_model_dir):
+                    os.makedirs(path_trained_model_dir)
+                raw_model = Model()
+                for attr_name in self.__dict__:
+                    try:
+                        setattr(raw_model, attr_name, copy.deepcopy(getattr(self, attr_name)))
+                    except:
+                        pass
+                joblib.dump(raw_model, path_trained_model)
 
-        def predict(self, X):
-            pass
-    
-        def predict_proba(self, X):
-            pass
 
-    return CachedModel(kwargs)
+    return CachedModel(**kwargs)
 
--- tests/test_cached_model.py
+++ tests/test_cached_model.py
@@ -6,3 +6,59 @@ Created on Tue Mar 20 17:40:45 2018
 """
 
 import unittest
+from cached_sklearn.model import create_cached_model
+from sklearn.svm import SVC
+from sklearn.base import clone
+import numpy as np
+import datetime
+
+class TestCachedModel(unittest.TestCase):
+
+    def test_model(self):
+        svc = SVC(C=10)
+        nsamples = 5000
+        nfeatures = 5
+        np.random.seed(0)
+        X = np.random.sample(size=(nsamples, nfeatures))
+        y = np.random.randint(2, size=nsamples)
+        start_time = datetime.datetime.now()
+        svc.fit(X, y)
+        delta_time = datetime.datetime.now() - start_time
+        print("fit using %d seconds." % delta_time.seconds) ## roughly 9 seconds
+        y_pred = svc.predict(X)
+
+        cached_svc1 = create_cached_model(SVC, C=10)
+        start_time = datetime.datetime.now()
+        cached_svc1.fit(X, y)
+        delta_time = datetime.datetime.now() - start_time
+        print("fit using %d seconds." % delta_time.seconds) ## roughly 9 seconds
+        cached_y_pred1 = cached_svc1.predict(X)
+
+        cached_svc2 = create_cached_model(SVC, C=10)
+        start_time = datetime.datetime.now()
+        cached_svc2.fit(X, y)
+        delta_time = datetime.datetime.now() - start_time
+        print("fit using %d seconds." % delta_time.seconds) ## less than 1 seconds
+        cached_y_pred2 = cached_svc2.predict(X)
+
+        self.assertTrue((y_pred == cached_y_pred1).all())
+        self.assertTrue((y_pred == cached_y_pred2).all())
+
+#    def test_copy(self):
+#        svc1 = SVC(C=10)
+#        nsamples = 100
+#        nfeatures = 5
+#        X = np.random.sample(size=(nsamples, nfeatures))
+#        y = np.random.randint(2, size=nsamples)
+#        svc1.fit(X, y)
+#        y_pred1 = svc1.predict(X)
+#
+#        svc2 = SVC()
+#        svc2 = clone(svc1)
+#        y_pred2 = svc2.predict(X)
+#        
+#        self.assertTrue((y_pred1 == y_pred2).all())
+        
+        
+if __name__ == '__main__':
+    unittest.main()
